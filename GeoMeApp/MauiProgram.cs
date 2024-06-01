c343794682c25e60ac2e6195e35b497962dd366c
vibara <vladimir.m.baranov@gmail.com>
2024-05-31 20:06:26 -0400

    The version works on Windows and Android, readme added

--- GeoMeApp/MauiProgram.cs
+++ GeoMeApp/MauiProgram.cs
@@ -12,20 +12,21 @@ namespace GeoMeApp
     {
 
         private const string DataFileName = "GeoMeData.db";
-        public static string DataFilePath { get; private set; } = Path.Combine(FileSystem.Current.AppDataDirectory, DataFileName); // example: GeoMeData "/data/user/0/com.companyname.geomeapp/files/GeoMeData.db"
+        public static string DataFilePath { get; private set; } = 
+            Path.Combine(FileSystem.Current.AppDataDirectory, DataFileName); // example: GeoMeData "/data/user/0/com.companyname.geomeapp/files/GeoMeData.db"
 
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                })
-                .UseMauiMaps();
-            AddConfiguration(builder);
+                });
+                AddConfiguration(builder);
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
@@ -52,7 +53,9 @@ namespace GeoMeApp
                 string keyAndroidGeoApiKey = configuration["AppSettings:Android_GeoApi_Key"] ?? string.Empty;
                 MetadataHelper.SetMetaDataValue("com.google.android.geo.API_KEY", keyAndroidGeoApiKey);
 #endif
-#if WINDOWS
+#if ANDROID || IOS 
+                builder.UseMauiMaps();
+#elif WINDOWS
                 builder.UseMauiCommunityToolkitMaps(keyMauiCommunityToolkit); // https://learn.microsoft.com/en-us/bingmaps/getting-started/bing-maps-dev-center-help/getting-a-bing-maps-key
 #endif
                 builder.Configuration.AddConfiguration(configuration);
---
+++ GeoMeApp/Readme.txt
@@ -0,0 +1,17 @@
+﻿
+
+In order to build a project, you need to add the file "appsettings.json" to the main folder of the project.
+
+The file should contain the following structure:
+
+{
+  "AppSettings": {
+    "BingMaps_Key": "BINGMAPS_KEY",
+    "Android_GeoApi_Key": "ANDROID_MAPS_KEY"
+  }
+}
+
+where:
+BINGMAPS_KEY - the key for Bing Maps (maps for Windows). You can create there: https://www.bingmapsportal.com/Application
+ANDROID_MAPS_KEY - the key for Android (Google) Maps. You can create it there: https://console.cloud.google.com/projectselector2/google/maps-apis/credentials?utm_source=Docs_CreateAPIKey&utm_content=Docs_maps-backend
+(documentation is there: https://developers.google.com/maps/documentation/javascript/get-api-key)
